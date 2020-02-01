14e09115037b2a6fd2e4f49686d6ceb014155098
Nelson Foltz <nelsfoltz@gmail.com>
2020-02-01 15:15:28 -0500

    add: ng-content with select

--- src/app/shared/divider/divider.component.ts
+++ src/app/shared/divider/divider.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./divider.component.css']
 })
 export class DividerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
