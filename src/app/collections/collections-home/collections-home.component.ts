38d07b45763205822a0920174c9c40d2875fea80
Nelson Foltz <nelsfoltz@gmail.com>
2020-02-01 16:58:40 -0500

    add: data arrays for table

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'James', age: 24, job: 'Designer' },
+    { name: 'Jill', age: 26, job: 'Engineer' },
+    { name: 'Elyse', age: 25, job: 'Engineer' }
+  ];
 
-  constructor() { }
+  header = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' }
+  ];
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
