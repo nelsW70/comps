100e272cc092a3d25dd12668aa30b65c33065b25
Nelson Foltz <nelsfoltz@gmail.com>
2020-02-02 10:09:44 -0500

    add: child components in collections and updated collections routing to include those children

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { PartnersComponent } from './partners/partners.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { BiographyComponent } from './biography/biography.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: CollectionsHomeComponent
+    component: CollectionsHomeComponent,
+    children: [
+      { path: '', component: BiographyComponent },
+      { path: 'companies', component: CompaniesComponent },
+      { path: 'partners', component: PartnersComponent }
+    ]
   }
 ];
 
