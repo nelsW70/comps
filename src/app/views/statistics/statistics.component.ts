2683ef5599566821cbe6101eb94031d5bbe0c25d
Nelson Foltz <nelsfoltz@gmail.com>
2020-02-02 15:09:41 -0500

    add: item component

--- src/app/views/statistics/statistics.component.ts
+++ src/app/views/statistics/statistics.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-statistics',
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistics.component.css']
 })
 export class StatisticsComponent implements OnInit {
+  @Input() data = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
